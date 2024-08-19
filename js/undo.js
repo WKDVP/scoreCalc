a59defcd33708a8705dc6828c3a2c1c1c5b8a6c5
WKDVP <jangkc1015@gmail.com>
2024-08-20 02:25:00 +0900

    undo 기능 추가

---
+++ js/undo.js
@@ -0,0 +1,21 @@
+function undo(isThree = false) {
+    let sivar = document.getElementById('p1l').innerHTML
+    let gaesivar = sivar.split('<br>').at(-2);
+    let sivarva = parseInt(document.getElementById('p1m').innerHTML);
+    document.getElementById('p1l').innerHTML = sivar.slice(0, sivar.lastIndexOf(gaesivar));
+    document.getElementById('p1m').innerHTML = sivarva - parseInt(gaesivar);
+
+    sivar = document.getElementById('p2l').innerHTML
+    gaesivar = sivar.split('<br>').at(-2);
+    sivarva = parseInt(document.getElementById('p2m').innerHTML);
+    document.getElementById('p2l').innerHTML = sivar.slice(0, sivar.lastIndexOf(gaesivar));
+    document.getElementById('p2m').innerHTML = sivarva - parseInt(gaesivar);
+    if(isThree){
+        sivar = document.getElementById('p3l').innerHTML;
+        gaesivar = sivar.split('<br>').at(-2);
+        sivarva = parseInt(document.getElementById('p3m').innerHTML);
+        document.getElementById('p3l').innerHTML = sivar.slice(0, sivar.lastIndexOf(gaesivar));
+        document.getElementById('p3m').innerHTML = sivarva - parseInt(gaesivar);
+    }
+    
+}
